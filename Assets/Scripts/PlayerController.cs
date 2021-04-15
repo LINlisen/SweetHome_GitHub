09aaccf723c15e89cdc59a364cb4e8c4bd25be77
Lisen <z1x2c3v441@gmail.com>
2021-04-15 16:16:10 +0800

    Pun2 Add

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] GameObject camerHolder;
+    [SerializeField] float mouseSensitivity, walkSpeed, jumpForce, smoothTime;
+	[SerializeField] Item[] items;
+
+	int itemIndex;
+	int previousItemIndex = -1; 
+
+    float verticalLookRotation;
+    bool grounded;
+    Vector3 smoothMoveVelocity;
+    Vector3 moveAmount;
+    
+    Rigidbody rb;
+
+	PhotonView PV;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+		PV = GetComponent<PhotonView>();
+	}
+
+    void Start()
+    {
+        if (PV.IsMine)
+        {
+			EquipItem(0); 
+        }
+        else
+        {
+			Destroy(GetComponentInChildren<Camera>().gameObject);
+			Destroy(rb);
+		}
+    }
+
+    private void Update()
+    {
+
+		if (!PV.IsMine)
+			return;
+		Look();
+		Move();
+		Jump();
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            if (Input.GetKeyDown((i + 1).ToString()))
+            {
+				EquipItem(i);
+				break;
+            }
+        }
+	}
+
+	void Jump()
+	{
+		if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+			rb.AddForce(transform.up* jumpForce);
+        }
+    }
+
+	void Move()
+	{
+
+		Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+		moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * walkSpeed, ref smoothMoveVelocity, smoothTime);
+	}
+
+    void Look()
+    {
+		transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+		verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+		verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+		camerHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+	}
+
+	void EquipItem(int _index)
+    {
+
+		if (_index == previousItemIndex)
+			return;
+
+		itemIndex = _index;
+
+		items[itemIndex].itemGameObject.SetActive(true);
+
+		if(previousItemIndex != -1)
+        {
+			items[previousItemIndex].itemGameObject.SetActive(false);
+        }
+
+		previousItemIndex = itemIndex;
+    }
+
+	public void SetGroundedState(bool _grounded)
+    {
+		grounded = _grounded;
+    }
+
+    private void FixedUpdate()
+    {
+		if (!PV.IsMine)
+			return;
+		rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+    }
+}
