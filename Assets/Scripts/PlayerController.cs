04281bf92d60853797d7c5e18d5c3076845b69da
Lisen <z1x2c3v441@gmail.com>
2021-04-16 11:59:29 +0800

    Color setting not finish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -149,9 +149,10 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.name == "BlueDoor")//only PlayerId==odd can pass
         {
 
-            if (this.GetComponent<TTPlayerState>().PlayerId % 2 == 0)
+            if (this.GetComponent<PhotonView>().ViewID % 2 == 0)
             {
-               // other.gameObject.SetActive(true);
+                // other.gameObject.SetActive(true);
+                this.gameObject.GetComponent<SpriteRenderer>().color = new Color(123, 250, 111, 100);
                 other.gameObject.transform.GetChild(3).gameObject.SetActive(true);
             }
             else
@@ -163,9 +164,10 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.name == "RedDoor")//only PlayerId==even can pass
         {
 
-            if (this.GetComponent<TTPlayerState>().PlayerId % 2 == 0)
+            if (this.GetComponent<PhotonView>().ViewID % 2 == 0)
             {
                 //other.gameObject.SetActive(false);
+                this.gameObject.GetComponent<MeshRenderer>().materials = Color.red;
                 other.gameObject.transform.GetChild(3).gameObject.SetActive(false);
             }
             else
