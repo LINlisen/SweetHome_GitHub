94496a0814011bcb0dc44da016f2e1cf4466952b
s110719001 <s110719001@stu.ntue.edu.tw>
2021-06-02 19:24:25 +0800

    Timer Complete Checked

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -1,9 +1,9 @@
-﻿using Photon.Pun;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
+using Photon.Pun;
 public class TimeController : MonoBehaviour
 {
     public int allSeconds;
@@ -30,10 +30,20 @@ public class TimeController : MonoBehaviour
         BlueTeam = 0;
         text_RT.text = RedTeam.ToString();
         text_BT.text = BlueTeam.ToString();
-        Hashtable time = PhotonNetwork.CurrentRoom.CustomProperties;
-        minutes = (int)time["Time"];
-        StartCoroutine(Timmer()); // 呼叫協程
-        
+        Hashtable time = new Hashtable();
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            allSeconds = (minutes * 60) + seconds;
+            time.Add("Time", allSeconds);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(time);
+        }
+        else
+        {
+            time = PhotonNetwork.CurrentRoom.CustomProperties;
+            allSeconds = (int)time["Time"];
+        }
+        StartCoroutine(Timmer(allSeconds)); // 呼叫協程
+
     }
 
 
@@ -44,22 +54,33 @@ public class TimeController : MonoBehaviour
         text_RT.text = RedTeam.ToString();
         text_BT.text = BlueTeam.ToString();
     }
-    IEnumerator Timmer()
+    IEnumerator Timmer(int currenttime)
     {
 
-        allSeconds = (minutes * 60) + seconds; //時間換算為秒數
-
-        text_Timmer.text = string.Format("{0}:{1}", minutes.ToString("00"), seconds.ToString("00")); //顯示一次最初的時間
+        /*allSeconds = (minutes * 60) + seconds;*/ //時間換算為秒數
+        Hashtable time = PhotonNetwork.CurrentRoom.CustomProperties;
+        /*text_Timmer.text = string.Format("{0}:{1}", minutes.ToString("00"), seconds.ToString("00"));*/ //顯示一次最初的時間
 
         //使用迴圈和 WaitForSeconds 來計秒
-
-        while (allSeconds > 0)
+        while (currenttime > 0)
         {
 
             yield return new WaitForSeconds(1); //每經過一秒
 
-            allSeconds--; //更改總合與顯示的秒數
-
+             //更改總合與顯示的秒數
+            if (PhotonNetwork.LocalPlayer.IsMasterClient)
+            {
+                currenttime--;
+                time["Time"] = currenttime;
+                PhotonNetwork.CurrentRoom.SetCustomProperties(time);
+            }
+            else
+            {
+                currenttime = (int)time["Time"];
+                Debug.Log((int)time["Time"]);
+                minutes = currenttime / 60;
+                seconds = currenttime % 60;
+            }
             seconds--; //換算
 
             if (seconds < 0)
