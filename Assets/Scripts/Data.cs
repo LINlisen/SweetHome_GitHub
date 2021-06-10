b751eb788402054c5980a2de5971ff1e1c335cf8
Lisen <z1x2c3v441@gmail.com>
2021-06-10 17:38:15 +0800

    Datasave

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 public class Data : MonoBehaviour
 {
+    public bool _bSeeSaw;
+    Hashtable hash = new Hashtable();
     // Start is called before the first frame update
     void Start()
     {
-        
+        _bSeeSaw = true;
+        hash.Add("seesawbool",_bSeeSaw);
+        PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
     }
 
     // Update is called once per frame
